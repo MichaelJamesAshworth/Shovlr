6eca32ab52ba601c90dddb813c353ac464383ee9
Matthew Litvinov <matthew.litvinov@gmail.com>
2022-02-11 22:15:27 -0500

    add db_helpers

--- express-backend/routes/users.js
+++ express-backend/routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+module.exports = (dbHelpers) => {
 
-module.exports = router;
+  router.get('/', (req, res) => {
+    dbHelpers.getUsers()
+      .then(users => res.json(users))
+      .catch(err => res.json({error: err.message}));
+  })
+
+  return router
+}
