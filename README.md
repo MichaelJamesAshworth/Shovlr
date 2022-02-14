a38566faea4b28721938b0d24a96180b8bdcf77d
michael <michael.james.ashworth@gmail.com>
2022-02-14 22:07:28 +0000

    Stripe API functions. Need to add CSS Styling. Add additional setup instructions to README for Stripe API dependencies.

--- README.md
+++ README.md
@@ -6,6 +6,11 @@ Shovlr is a simple, easy to use application that is rendered via React. It's an 
 
 Install dependencies with `npm install`.
 
+You may need to install Stripe API's dependencies manually: 
+
+- Within the front-end directory (SHOVLR/client) run `npm install --save @stripe/react-stripe-js @stripe/stripe-js` 
+- Within the back-end directory (SHOVLR/express-backend) run `npm install --save stripe`
+
 ## Backend
 
 Start the backend server with `npm start`.
--- express-backend/routes/index.js
+++ express-backend/routes/index.js
@@ -1,42 +1,29 @@
 const express = require("express");
 const router = express.Router();
-const stripe = require("stripe");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY); // <-- change the key here
 
 /* GET home page. */
-//code within this route is just to make sure front-end was communicating with back-end:
-router.get("/", function (req, res) {
-  console.log("hello from index");
-  res.send("Communication test for front to back-end: data from index.js line 9");
+/*The commented code within this route was just to make sure front-end was communicating with
+back-end. The corresponding commented code is within app.jsx lines 24-41. Left in case we needed to check again */
+router.get("/", function(req, res) {
+  // console.log("hello from index");
+  // res.send("Communication test for front to back-end: data from index.js line 9");
 });
 
-router.post("pay", async (req, res) => {
-  const { email } = req.body;
 
-  const paymentIntent = await stripe.paymentIntent.create({
-    amount: 5000,
+router.post("/create-payment-intent", async(req, res) => {
+  const { email, price } = req.body;
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: price,
     currency: "cad",
     //Verify your integration in this guide by including this parameter
     metadata: { integrationCheck: "accept a payment" },
     recepientEmail: email,
   });
-  res.json({ "client_secret": paymentIntent["client_secret"] });
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
 });
-
-// .then version of above:
-// router.post("pay", (req, res) => {
-//   const { email } = req.body;
-
-//   stripe.paymentIntent
-//     .create({
-//       amount: 5000,
-//       currency: "cad",
-//       //Verify your integration in this guide by including this parameter
-//       metadata: { integrationCheck: "accept a payment" },
-//       recepientEmail: email,
-//     })
-//     .then((paymentIntent) => {
-//       res.json({ client_secret: paymentIntent["client_secret"] });
-//     });
-// });
 
 module.exports = router;
