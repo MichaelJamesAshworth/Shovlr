f5bda174ad61e3583fd19d9c92dcd02b1e51068b
michael <michael.james.ashworth@gmail.com>
2022-02-14 17:11:49 +0000

    Alter front-end and back-end to communicate correctly. Add additional dependancies. Run NPM I again

--- express-backend/app.js
+++ express-backend/app.js
@@ -1,37 +1,38 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const db = require('./db');
-const dbHelpers = require('./helpers/db_helpers')(db);
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const db = require("./db");
+const dbHelpers = require("./helpers/db_helpers")(db);
 const http = require("http");
+const cors = require('cors');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
 const app = express();
-const server = http.createServer(app)
+const server = http.createServer(app);
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({ origin: true, credentials: true }));
 
-app.use('/', indexRouter);
-app.use('/api/users', usersRouter(dbHelpers));
+app.use("/", indexRouter);
+app.use("/api/users", usersRouter(dbHelpers));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
@@ -42,5 +43,5 @@ module.exports = app;
 const port = 3001;
 
 server.listen(port, () => {
-  console.log(`Server is listening on port ---->:${port}`);
+  console.log(`Server is listening on port: ${port}`);
 });
