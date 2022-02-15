91ff96e371fda4de326b648e27a99e862ffe9039
Eric Moolenbeek <emoolenbeek@gmail.com>
2022-02-15 16:49:45 +0000

    add maps api and click to add markers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
 import CheckoutForm from './components/CheckoutForm';
+import React, { useState } from 'react';
+
+// Google map react api
+import { 
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow
+} from "@react-google-maps/api";
+
+// map styling
+import mapStyles from './mapStyles';
 
 //Stripe related imports
 import {Elements} from '@stripe/react-stripe-js';
@@ -10,6 +22,23 @@ import {loadStripe} from '@stripe/stripe-js';
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
 const stripePromise = loadStripe('pk_test_51KPUvgLzGdhWWQN43gX8AKiDzqiAUPab5CLLzJQ3VmggDKuBbmmA6aknytp7B8zOAolhN4vfZMLpAaRJ8Jva2EQu00CMiUU59f');
+
+// Maps api variables
+const libraries = ["places"];
+const mapContainerStyle = {
+  width: '100vw',
+  height: '100vh'
+};
+const center = {
+  lat:  45.424721,
+  lng: -75.695000
+};
+
+const options = {
+  styles: mapStyles,
+  disableDefaultUI: true,
+  zoomControl: true
+};
 
 // function App() {
 //   const options = {
@@ -28,6 +57,27 @@ const stripePromise = loadStripe('pk_test_51KPUvgLzGdhWWQN43gX8AKiDzqiAUPab5CLLz
 
 //Sample to test that front-end and back-end is communicating properly:
 function App() {
+  const {isLoaded, loadError} = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries
+  });
+
+  const [markers, setMarkers] = useState([]);
+  const onMapClick = React.useCallback((event) => {
+    setMarkers(current => [...current, {
+      lat: event.latLng.lat(),
+      lng: event.latLng.lng(),
+      time: new Date()
+    }]) 
+  }, []);
+
+  const mapRef = new React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  if (loadError) return "Error loading maps";
+  if (!isLoaded) return "Loading maps"; 
 
   const onSubmit = () => {
     return axios.get("http://localhost:3001", )
@@ -40,7 +90,28 @@ function App() {
   }
   
   return (
-    <button onClick={onSubmit}>Submit</button>
+    <>
+      <h1 className="brand">Shovlr</h1>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle} 
+        zoom={9}
+        center={center}
+        options={options}
+        onClick={onMapClick}
+        onLoad={onMapLoad}
+        >
+          {markers.map(marker => <Marker 
+            key={marker.time.toISOString()} 
+            position={{ lat: marker.lat, lng: marker.lng }}
+            icon={{
+              url: '/shovel.svg',
+              scaledSize: new window.google.maps.Size(30,30),
+
+            }}
+          />)}
+        </GoogleMap>
+      <button onClick={onSubmit}>Submit</button>
+    </>
   );
 };
 
---
+++ client/src/mapStyles.js
@@ -0,0 +1,164 @@
+export default [
+  {
+      "featureType": "all",
+      "elementType": "labels.text.fill",
+      "stylers": [
+          {
+              "color": "#667d8d"
+          },
+          {
+              "saturation": "0"
+          }
+      ]
+  },
+  {
+      "featureType": "landscape",
+      "elementType": "all",
+      "stylers": [
+          {
+              "color": "#ffffff"
+          }
+      ]
+  },
+  {
+      "featureType": "poi",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "color": "#e8edf0"
+          }
+      ]
+  },
+  {
+      "featureType": "poi.attraction",
+      "elementType": "labels.icon",
+      "stylers": [
+          {
+              "saturation": "-100"
+          }
+      ]
+  },
+  {
+      "featureType": "poi.business",
+      "elementType": "all",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
+  },
+  {
+      "featureType": "poi.government",
+      "elementType": "labels.icon",
+      "stylers": [
+          {
+              "saturation": "-100"
+          }
+      ]
+  },
+  {
+      "featureType": "poi.park",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "color": "#bae8aa"
+          },
+          {
+              "saturation": "0"
+          }
+      ]
+  },
+  {
+      "featureType": "poi.place_of_worship",
+      "elementType": "labels.icon",
+      "stylers": [
+          {
+              "saturation": "-100"
+          }
+      ]
+  },
+  {
+      "featureType": "poi.school",
+      "elementType": "labels.icon",
+      "stylers": [
+          {
+              "saturation": "-100"
+          }
+      ]
+  },
+  {
+      "featureType": "poi.sports_complex",
+      "elementType": "labels.icon",
+      "stylers": [
+          {
+              "saturation": "-100"
+          }
+      ]
+  },
+  {
+      "featureType": "road",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "color": "#e2e8ea"
+          }
+      ]
+  },
+  {
+      "featureType": "road.highway",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "color": "#c2d1d6"
+          }
+      ]
+  },
+  {
+      "featureType": "road.highway",
+      "elementType": "geometry.stroke",
+      "stylers": [
+          {
+              "color": "#a9b4b8"
+          },
+          {
+              "visibility": "on"
+          }
+      ]
+  },
+  {
+      "featureType": "road.highway",
+      "elementType": "labels.text.fill",
+      "stylers": [
+          {
+              "color": "#3c5566"
+          }
+      ]
+  },
+  {
+      "featureType": "road.local",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "color": "#f0f0f2"
+          }
+      ]
+  },
+  {
+      "featureType": "transit.station.airport",
+      "elementType": "geometry.fill",
+      "stylers": [
+          {
+              "color": "#e8edf0"
+          }
+      ]
+  },
+  {
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "color": "#88d5f4"
+          }
+      ]
+  }
+]
