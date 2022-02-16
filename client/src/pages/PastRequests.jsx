f44b9531cec418ba620bb4ff08074614cd331542
Eric Moolenbeek <emoolenbeek@gmail.com>
2022-02-16 17:32:51 -0500

    create pastRequest component

--- client/src/pages/PastRequests.jsx
+++ client/src/pages/PastRequests.jsx
@@ -1,9 +1,28 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import PastRequest from '../components/PastRequest';
+
 const PastRequests = () => {
+
+  // state
+  const [requests, setRequests] = useState(null);
+
+  // hook
+  useEffect(() => {
+    Promise.all([
+      axios.get('/api/removal_requests/completed/1'),
+    ]).then((all) => {
+      setRequests(all[0].data);
+    });
+  }, []);
+
   return (
     <div className="PastRequests">
       <h2>Past requests</h2>
+
+      { requests && <PastRequest requests={requests} />}
     </div>
   );
 }
 
-export default PastRequests
+export default PastRequests;
