2cc3462df8dc2fc9f70e0b4f0c0f0a39c0eef230
Eric Moolenbeek <emoolenbeek@gmail.com>
2022-02-18 13:54:39 -0500

    add ongoing request button to account page

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -5,8 +5,12 @@ const Account = () => {
     <div className='Account'>
       <h2>Account</h2>
       <nav>
+        <Link to='../Status'>
+          <button type="button" className="btn btn-success account-button">View ongoing request</button>
+        </Link>
+        <br/>
         <Link to='PastRequests'>
-          <button type="button" className="btn btn-primary">View past requests</button>
+          <button type="button" className="btn btn-primary account-button">View past requests</button>
         </Link>
       </nav>
       <p className="foot-text">Shovlr 2022</p>
