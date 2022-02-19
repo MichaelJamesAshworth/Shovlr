f87ac3150ca69b18a0224d326b864f15459803fe
Eric Moolenbeek <emoolenbeek@gmail.com>
2022-02-19 15:44:27 -0500

    add set timeout to get data

--- snow-remover-client/src/pages/Home.jsx
+++ snow-remover-client/src/pages/Home.jsx
@@ -4,19 +4,36 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   const [requests, setRequests] = useState([]);
+
+  // solution 1
+  // websockets
+  // socket.io
+
+  // when something triggered on the server - client can react based on that
+
+  // solution 2
+  // polling - periodically checks
+  // set timeout
+
+
   useEffect(() => {
+    getAllRequests();
+  }, []);
+
+  const getAllRequests = () => {
     axios({
       method: 'GET',
       url: '/api/removal_requests'
     })
     .then(result => {
-      console.log(result.data)
-      setRequests(result.data)
+      console.log(result.data);
+      setRequests(result.data);
+      // setTimeout(getAllRequests, 1000);
     })
     .catch(error => {
       console.log('/api/removal/requests error')
-    })
-  }, [])
+    });
+  }
 
   const calculatePrice = (size) => {
     var formatter = new Intl.NumberFormat('en-US', {
@@ -42,24 +59,26 @@ const Home = () => {
     if (request.started_at === null && request.completed_at === null) {
       
       return (
-        <div className="card">
-          <div className="card-body">
-            <p className="card-text">{request.note}</p>
-            <p className="card-text">Address: {request.address_id}</p>
-            <p className="card-text">{calculatePrice(request.total_cents)}</p>
-            <Link to={{
-                pathname: '/ActiveRequest/' + request.id,
-              }} onClick={() => markStarted(request.id)}><button type="button" class="btn btn-success">Accept</button>
-            </Link>
+        <li key={request.id}>
+          <div className="card">
+            <div className="card-body">
+              <p className="card-text">{request.note}</p>
+              <p className="card-text">{request.address}</p>
+              <p className="card-text">{calculatePrice(request.total_cents)}</p>
+              <Link to={{
+                  pathname: '/ActiveRequest/' + request.id,
+                }} onClick={() => markStarted(request.id)} className='navbar-brand'><button type="button" class="btn btn-success">Accept</button>
+              </Link>
+            </div>
           </div>
-        </div>
+        </li>
       )
     }
   })
   return (
     <div className='Home'>
       <h1>Jobs available</h1>
-      {requestList}
+      {requestList.reverse()}
     </div>
   );
 }
