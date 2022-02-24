2a3f65da4a3137b534e945493e0b0492bef1f593
Eric Moolenbeek <emoolenbeek@gmail.com>
2022-02-24 14:18:36 -0500

    fix typo

--- client/src/components/RequestRemovalButton.jsx
+++ client/src/components/RequestRemovalButton.jsx
@@ -4,7 +4,7 @@ const RequestRemovalButton = () => {
 
   return (
     <Link to='Checkout' className='requestButton'>
-      <button type="button" className="btn">Requst snow removal</button>
+      <button type="button" className="btn">Request snow removal</button>
     </Link>
   );
 }
